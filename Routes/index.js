5cc682d60b068c7f74f85d998f2f1901149a44ec
manraj singh <vmanrajsingh@gmail.com>
2023-05-19 20:04:19 +0530

    final commit

--- Routes/index.js
+++ Routes/index.js
@@ -7,7 +7,7 @@ router.use("*", (req, res) => {
   return res.status(404).json({
     status: false,
     messag: "404 , wrong endpoint",
-    data: " endpoints are '/api/user/signup ' , '/api/user/generate-otp ' , '/api/user/verify ' ",
+    data: " POST endpoints are: '/api/user/signup ' , '/api/user/generate-otp ' , '/api/user/verify ' ",
   });
 });
 module.exports = router;
